8c88877ddbccec89d3de321d18b966be2d32cdfd
Arild Wanvik Tvergrov <arild.wanvik@gmail.com>
2017-11-17 16:12:41 +0100

    Build library with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'hateoasForm.js',
+    library: 'hateoasForm',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: '_'
+    }
+  }
+};
